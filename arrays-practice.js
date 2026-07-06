6d44559db5ecb358bd0730c6eab093c13feaea50
Jun Kim <hojunkim2@gmail.com>
2026-07-05 23:38:15 -0700

    week 3 practice: indexOf(), splice(), includes(), and methods for creating shallow copies of arrays

--- arrays-practice.js
+++ arrays-practice.js
@@ -35,3 +35,37 @@ console.log(reversedName);
 // reversing a string in one line of code
 let reversedName2 = name.split("").reverse().join("");
 console.log(reversedName2);
+
+// indexOf() method
+// array.indexOf(element, startingIndex)
+// discoveries: case sensitive and if the value doesn't exist, returns -1
+const bananaIndex = fruits.indexOf("banana");
+const raspberryIndex = fruits.indexOf("raspberry");
+console.log(bananaIndex);
+console.log(raspberryIndex);
+
+// shallow copies using concat(), slice(), and spread operator
+// discoveries: shallow copies are not exact copies of the original array, equality check returns false
+const copyFruits1 = [].concat(fruits);
+const copyFruits2 = fruits.slice();
+const copyFruits3 = [...fruits];
+
+console.log(copyFruits1);
+console.log(copyFruits2);
+console.log(copyFruits3);
+console.log(
+   copyFruits1 === fruits || copyFruits2 === fruits || copyFruits3 === fruits,
+);
+
+// splice() method for adding and removing elements in middle of array
+// array.splice(startingIndex, numItemsToRemove, addItem1, addItem2)
+// discoveries: mutates original array
+const removed = copyFruits1.splice(2, 2, "kumquat", "pineapple", "mango");
+console.log(removed);
+console.log(copyFruits1);
+
+// includes() method
+// array.includes(element, startingIndex)
+// discoveries: case sensitive and uses strict equality comparison
+console.log(fruits.includes("banana"));
+console.log(fruits.includes("watermelon"));
