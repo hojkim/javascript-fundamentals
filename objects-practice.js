99b6f72e6edd389c0e387b8aa65ffead83b5dece
Jun Kim <hojunkim2@gmail.com>
2026-07-09 23:48:33 -0700

    week 4 practice: objects, accessing and deleting  properties, and ways to check if property exists

---
+++ objects-practice.js
@@ -0,0 +1,36 @@
+const person = {
+   name: "Hojun",
+   age: 28,
+   nickname: "Nuj",
+   hairColor: "Black",
+   "native descent": "South Korean",
+   contact: {
+      email: "junjunjun@email.com",
+      phone: {
+         home: "123-456-7890",
+         work: "098-765-4321",
+      },
+   },
+   addresses: [
+      { type: "work", street: "100 Main St", city: "Montyville" },
+      { type: "home", street: "999 Densil Ave", city: "Northshore" },
+   ],
+};
+
+console.log(person.nickname);
+console.log(person["native descent"]);
+console.log(person.contact.phone.work);
+console.log(person["contact"]["phone"]["home"]);
+console.log(person.addresses[0].city);
+
+// delete operator deletes properties from an object
+delete person.hairColor;
+console.log(person.hairColor);
+
+// Testing different options to check if an object has a property
+console.log(Object.hasOwn(person, "name"));
+console.log(Object.hasOwn(person, "hairColor"));
+console.log(Object.hasOwn(person, "naMe"));
+console.log("name" in person);
+console.log(person.name !== undefined);
+console.log(person.hairColor !== undefined);
