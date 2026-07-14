9411f17dc1ec046d3331a0e078b942bc77d2d9b8
Jun Kim <hojunkim2@gmail.com>
2026-07-13 22:46:13 -0700

    week 4: object methods

--- objects-practice.js
+++ objects-practice.js
@@ -2,7 +2,7 @@ const person = {
    name: "Hojun",
    age: 28,
    nickname: "Nuj",
-   hairColor: "Black",
+   hairColor: "black",
    "native descent": "South Korean",
    contact: {
       email: "junjunjun@email.com",
@@ -15,6 +15,12 @@ const person = {
       { type: "work", street: "100 Main St", city: "Montyville" },
       { type: "home", street: "999 Densil Ave", city: "Northshore" },
    ],
+   greeting: function () {
+      return `Hello, my name is ${this.name} and I am ${this["native descent"]}.`;
+   },
+   workPhone() {
+      return `My work phone number is ${this.contact.phone.work}.`;
+   },
 };
 
 console.log(person.nickname);
@@ -34,3 +40,5 @@ console.log(Object.hasOwn(person, "naMe"));
 console.log("name" in person);
 console.log(person.name !== undefined);
 console.log(person.hairColor !== undefined);
+console.log(person.greeting());
+console.log(person.workPhone());
