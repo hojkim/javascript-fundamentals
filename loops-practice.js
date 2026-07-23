6b6f4401ba461fc531b5c116e862b85e5425282f
Jun Kim <hojunkim2@gmail.com>
2026-07-22 23:06:35 -0700

    week 5 practice: for...in loop for looping through the properties of an object

--- loops-practice.js
+++ loops-practice.js
@@ -25,3 +25,39 @@ console.log(name);
 for (const char of name) {
    console.log(char.toUpperCase());
 }
+
+const address = {
+   houseNumber: 9204,
+   street: "McKinney Ave NE",
+   city: "Bennyville",
+   state: "Arkansas",
+   zip: 71620,
+};
+
+// for...in loop - useful for looping over objects
+let fullAddress = "";
+for (const prop in address) {
+   fullAddress = fullAddress.concat(" ", address[prop]);
+}
+console.log(fullAddress.trimStart());
+
+const person = {
+   name: "Nuj",
+   age: 28,
+   address: address,
+};
+
+// object validator - checks arrays and null values since both are considered objects using typeof
+function isObject(obj) {
+   return typeof obj === "object" && !Array.isArray(obj) && typeof obj !== null;
+}
+
+for (const prop in person) {
+   if (isObject(person[prop])) {
+      for (const nestedProp in person[prop]) {
+         console.log(person[prop][nestedProp]);
+      }
+   } else {
+      console.log(person[prop]);
+   }
+}
