e0d6a06b18d18c7ec1520b73adbe2fc3341e45f8
Jun Kim <hojunkim2@gmail.com>
2026-08-09 20:56:35 -0700

    week 6 review: first element function

--- fundamentals-practice.js
+++ fundamentals-practice.js
@@ -1,3 +1,4 @@
+// Closures
 function createDecrementer() {
    let count = 100;
    return function () {
@@ -20,3 +21,30 @@ function multiply(x) {
 let multiply10 = multiply(10);
 console.log(multiply10(4));
 console.log(multiply10(200));
+
+// This function returns the first element in a passed array that passes the passed test function
+// If none of the elements in the array pass the test function, it returns undefined
+const findElement = (arr, func) => {
+   for (const num of arr) {
+      if (func(num)) {
+         return num;
+      }
+   }
+   return undefined;
+};
+
+console.log(
+   findElement([1, 3, 5, 8, 9, 10], function (num) {
+      return num % 2 === 0;
+   }),
+);
+console.log(
+   findElement(["hello", "world", "javascript"], function (str) {
+      return str.length > 5;
+   }),
+);
+console.log(
+   findElement(["cat", "dog", "bird"], function (str) {
+      return str.length > 10;
+   }),
+);
